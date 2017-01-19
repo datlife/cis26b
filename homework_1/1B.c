a031e7ab3e823d68b80fdcb1efe9b1d05f2c5568
Dat Nguyen <tdat.nguyen93>
2017-01-18 22:54:56 -0800

    Fixed IO issue

--- homework_1/1B.c
+++ homework_1/1B.c
@@ -33,6 +33,7 @@ void   save_file(char  *filename, STOCK *file, int size);
 void   print_list(char title[], const STOCK *list, int size, char format[]);
 
 // Helper functions
+char  *get_input(char *message);
 void   sort_by_name(STOCK *new_stock, STOCK *list, int size);
 int    found_duplicate(char stock_name[], const STOCK *list, int size);
 void   write_data(FILE *fp, char *filename, STOCK* list, int size);
@@ -42,10 +43,15 @@ int main(void)
 	int    arr_size = 0;
 	STOCK *arr_stock = NULL;
 
-	arr_stock = read_file(INPUT_PATH, &arr_size);						 // Read input file 
+
+	char  *input_file = get_input("Enter your input file (or press Enter for default choice):");
+
+	arr_stock = read_file(input_file, &arr_size);						 // Read input file 
 	if (arr_stock) {
 		print_list("Stock Summary", arr_stock, arr_size, FORMAT_DESC);   // Display to screen
-		save_file(OUTPUT_PATH, arr_stock, arr_size);					 // Save file into output_file
+
+		char  *output_file = get_input("Enter your output file (or press Enter for default choice):");
+		save_file(output_file, arr_stock, arr_size);					 // Save file into output_file
 		free(arr_stock);												 // Release memory
 	}
 	return 0;
@@ -54,7 +60,7 @@ int main(void)
 /*===============================================================
 * read_file()
 * ===============================================================
-* Read input file and sorted data while reading input
+* Read input file and sort data while reading input
 * 	Pre: filename	 : name of current stock
 * 	     arr_size    : number of stocks in the list
 *
@@ -67,10 +73,16 @@ STOCK *read_file(char  *filename, int *arr_size) {
 	STOCK *arr_stock = NULL;
 
 	//Open a file
-	if (!(fp = fopen(filename, "r+"))){
-		printf("File not found\n");
+	if (strcmp(filename, "") != 0)
+		fp = fopen(filename, "r");
+	else
+		fp = fopen(INPUT_PATH, "r");
+
+	if (!fp) {
+		printf("File %s not found\n", INPUT_PATH);
 		exit(101);
 	}
+
 
 	int count = 0;  								// store the loop counts = number of lines in file
 	rewind(fp); 									// Make sure we are at first pos
@@ -103,7 +115,7 @@ STOCK *read_file(char  *filename, int *arr_size) {
 		//Copy size of list to arr_size
 		*arr_size = size;
 	}
-	if (fclose(fp) == EOF){
+	if (fclose(fp) == EOF) {
 		printf("Error closing file %s", filename);
 		exit(201);
 	}
@@ -121,17 +133,26 @@ void   save_file(char  *filename, STOCK *file, int size) {
 	// Open file in writing mode, remmber using wx
 	FILE  *fp;
 	//Open a file
-	fp = fopen(filename, "r");
+	//Open a file
+	if (strcmp(filename, "") != 0)
+		fp = fopen(filename, "r");
+	else {
+		fp = fopen(OUTPUT_PATH, "r");
+		strcpy(filename, OUTPUT_PATH);
+	}
+
 	if (!fp) { // File is not created yet
+		fp = fopen(filename, "w");
 		write_data(fp, filename, file, size);
 	}
-	else{     // File is existed
+	else {     // File is existed
 		char choice;
-		while (1){
+		while (1) {
 			printf("File %s is existed. Do you want to overwrite? (y/n)", filename);
 			scanf("%c", &choice);
-			if (toupper(choice) == 'Y'){
-				write_data(fp, filename, file, size); 
+			if (toupper(choice) == 'Y') {
+				fp = fopen(filename, "w");
+				write_data(fp, filename, file, size);
 				break;
 			}
 			else if (toupper(choice) == 'N') break;
@@ -139,9 +160,9 @@ void   save_file(char  *filename, STOCK *file, int size) {
 				printf("\nInvalid choice. Please type again\n");
 				FLUSH;
 			}
-		}	
+		}
 	}
-	if (fclose(fp) == EOF){
+	if (fclose(fp) == EOF) {
 		printf("Error closing file %s", filename);
 		exit(201);
 	}
@@ -152,10 +173,9 @@ void   save_file(char  *filename, STOCK *file, int size) {
 * To avoid write codes twice in save_file().
 *	Post: write data to a file
 */
-void   write_data(FILE *fp, char *filename, STOCK* file, int size){
-	fp = fopen(filename, "w");
+void   write_data(FILE *fp, char *filename, STOCK* file, int size) {
 	fprintf(fp, "%d\n", size);
-	for (int i = 0; i < size; i++){
+	for (int i = 0; i < size; i++) {
 		fprintf(fp, "%-6s %-4d\n", file->name, file->shares);
 		file++;
 	}
@@ -171,37 +191,26 @@ void   write_data(FILE *fp, char *filename, STOCK* file, int size){
 *	Post: sorted array
 */
 void   sort_by_name(STOCK *new_stock, STOCK *list, int size) {
-	if (size == 0){
-		strcpy(list[0].name, new_stock->name);
-		list[0].shares = new_stock->shares;
-	}
-	else{
-		STOCK *p_walk = list;
-		while(p_walk != (list + size)){
-			if (strcmp(new_stock->name, p_walk->name) <= 0){
-				STOCK p_prev;
-				strcpy(p_prev.name, p_walk->name); p_prev.shares = p_walk->shares;
-				// Shift array by one element
-				for (STOCK *p_curr = p_walk + 1; p_curr != (list + size + 1); p_curr++){
-					// Swapping
-					STOCK temp;
-					strcpy(temp.name, p_curr->name);   temp.shares = p_curr->shares;	
-					strcpy(p_curr->name, p_prev.name); p_curr->shares = p_prev.shares;
-					strcpy(p_prev.name, temp.name);    p_prev.shares = temp.shares;   											
-				}
-				// Copy new stock
-				strcpy(p_walk->name, new_stock->name);
-				p_walk->shares = new_stock->shares;
-				break;
+	STOCK *p_walk = list;
+	while (p_walk != (list + size)) {
+		if (strcmp(new_stock->name, p_walk->name) <= 0) {
+			STOCK p_prev;
+			strcpy(p_prev.name, p_walk->name); p_prev.shares = p_walk->shares;
+			// Shift array by one element
+			for (STOCK *p_curr = p_walk + 1; p_curr != (list + size + 1); p_curr++) {
+				// Swapping
+				STOCK temp;
+				strcpy(temp.name, p_curr->name);   temp.shares = p_curr->shares;
+				strcpy(p_curr->name, p_prev.name); p_curr->shares = p_prev.shares;
+				strcpy(p_prev.name, temp.name);    p_prev.shares = temp.shares;
 			}
-			p_walk++;			
+			break;
 		}
-		if (p_walk == list + size){
-			// Copy new stock
-			strcpy(p_walk->name, new_stock->name);
-			p_walk->shares = new_stock->shares;
-		}
+		p_walk++;
 	}
+	// Copy new stock
+	strcpy(p_walk->name, new_stock->name);
+	p_walk->shares = new_stock->shares;
 }
 /*===============================================================
 * print_list()
@@ -243,3 +252,42 @@ int   found_duplicate(char name[], const STOCK *arrStock, int size) {
 	}
 	return found;
 }
+char *get_input(char *message) {
+	char *fname = (char*)malloc(MAX_LEN);
+	printf(message);
+	fgets(fname, MAX_LEN, stdin);
+	fname[strcspn(fname, "\n")] = 0;
+	return fname;
+}
+
+/*
+=============================SAMPLE OUTPUT==============================
+
+Enter your input file (or press Enter for default choice):test.txt
+'test.txt' is not found. Using default filename './in.txt'...
+
+Stock Summary
+
+AAL       369
+AAPL     2879
+BBBY      471
+CHTR      423
+EBAY      899
+GOOG      123
+GOOGL     123
+MSFT      380
+NLFX     1474
+SIRI     1908
+TSLA     1383
+XLNX      332
+XLNX18     89
+YHOO      458
+
+Enter your output file (or press Enter for default choice):
+File ./out.txt is existed. Do you want to overwrite? (y/n)s
+
+Invalid choice. Please type again
+File ./out.txt is existed. Do you want to overwrite? (y/n)y
+
+File is saved to './out.txt'
+*/
