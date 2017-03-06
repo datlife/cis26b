ebc1dc176a53fdfd939966073d3b5a2fe10bd412
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-03-05 17:39:25 -0800

    Completed Part B

--- homework4/26B_NGUYEN_DAT_HW4/26B_NGUYEN_DAT_HW4/main.c
+++ homework4/26B_NGUYEN_DAT_HW4/26B_NGUYEN_DAT_HW4/main.c
@@ -1,32 +1,46 @@
-
+/***
+CIS 26B: Homework 4 - Bit Manipulation
+***************************************
+NAME: Dat Nguyen
+IDE (compiler): Visual Studio 2015
+*************************************************************************
+*/
+# define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
 #define NUM_LIGHTS   16
+#define NUMERIC      "0123456789"
+#define INPUT_BUFFER 20
 #define TURN_ON_BITS(startbit,  numbits)		(unsigned short)((~(((unsigned short)~0) << (numbits))) << (startbit))		// turn on a range of bits
 #define TURN_OFF_BITS(startbit,numbits)			(unsigned short)(~((~(((unsigned short) ~0) << (numbits))) << (startbit)))  //turn off a range of bits
 #define TOGGLE_BITS(bits, startbit, numbits)    (unsigned short)( (bits)^TURN_ON_BITS((startbit), (numbits)) )					// toggle a range of bit
 #define FLUSH									while(getchar() != '\n')
-#define NUMERIC   "0123456789"
 
 int main(int args, char argv[])
 {
-	// ------FUNCTION PROTOTYPES--------
+	//==========================FUNCTION PROTOTYPES==========================//
 	void menu_driver(unsigned short);
-	int  update(unsigned short*, int choice);
-	void overlay(unsigned short*, unsigned short);
+	void update(unsigned short*, int choice);
+	void overlay(unsigned short*);
 	void printbits(unsigned short);
+	int validate_input(char *, int* startbit, int* numbit, unsigned short* pattern);
+	int is_digit(const char*);
+	extern int isspace(char);   // avoid warning C4013 on VS 2013, ": undefined; assuming extern returning int"
+	extern int isdigit(char); 
 
-	//------ MAIN PROGRAM --------------
+	//========================== MAIN  =============================//
 	unsigned short current_lighting = 0; //set all lights to OFF
 	menu_driver(current_lighting);
 	return 0;
 }
-
+/********************************************
+Display menu  for user to select
+*/
 void menu_driver(unsigned short lights) {
-	char input[3];
+	char input[INPUT_BUFFER];
 	int choice = -1;
 	do {
 		printf("==================== LIGHTING SYSTEM ===================\n");
@@ -41,25 +55,27 @@ void menu_driver(unsigned short lights) {
 		printf(" 9) Overlay ON/OFF pattern onto light configuration.\n");
 		printf(" 10) Quit.\n");
 		printf("=========================================================\n");
-		printf("Please select an option: ");
-		fgets(input, 3, stdin);
-		if ((isdigit(input[0]) && strlen(input) == 2)) {
-			choice = strtol(input, NULL, 0); // Convert to number
-			printf("OPTION: %d\n\n", choice);
+		printf("Enter menu choice: ");
+		fgets(input, INPUT_BUFFER, stdin);
+		strtok(input, "\n");
+		if (is_digit(input) == 0)  {
+			choice = (int)strtol(input, NULL, 0); // Convert to number
 			if (choice == 10) break;
 			if (1 <= choice && choice < 10)
 				update(&lights, choice);
-			else
-				printf("\n**Option %d is  an invalid choice, please try again**\n", choice);
+			else {
+				printf("Illegal menu selection! Try again!\n");
+			}
 		}
 		else {
-			printf("\n**Invalid Input, please try again.**\n");
-			FLUSH; 
+			printf("Illegal menu selection! Try again!\n");
 		}
 	} while (choice != 10);
 }
-int update(unsigned short* lights, int choice) {
-	int okay = 0;
+/***********************************************************
+Update lighting system
+*/
+void update(unsigned short* lights, int choice) {
 	switch (choice)
 	{
 	case 1: // Turn all all lights
@@ -87,37 +103,98 @@ int update(unsigned short* lights, int choice) {
 		*lights &= TURN_OFF_BITS(0, 5);
 		break;
 	case 9:
-		printf("Update a custom configuration (pattern - location to start): ");
-		char input[16];		 // why?
-		fgets(input, 16, stdin); // ERROR CHECK HERE
-		int okay = validate_input(input);
-		unsigned short pattern = (unsigned short)strtol(input, NULL, 0);
-		overlay(lights, pattern);
+		overlay(lights);
 		break;
 	default:
 		break;
 	}
-	printf("Current light configuration (1-ON / 0-OFF): \n");
-	printbits(*lights);
-	return okay;
+	printf("Light settings: ");
+	printbits(*lights);	
 }
-int validate_input(char *input) {
-
-	char *start_bit = strtok(input, " ");
-	char *pattern = strtok(input, "\t\n");
-	int id, pattern;
-	if (start_bit) {
-		start_bit[strspn(start_bit, NUMERIC)] = 0;
-		strtol(start_bit, NULL, 0);
+/**********************************************************8**
+Overlay a bit pattern
+*/
+void overlay(unsigned short* lights) {
+	int okay = -1;
+	do {
+		printf("Enter starting light number and on/off pattern: ");
+		char input[16];		 // why?
+		fgets(input, 16, stdin); // ERROR CHECK HERE
+		int starting_light;
+		int numbits;
+		unsigned short pattern;
+		okay = validate_input(input, &starting_light, &numbits, &pattern);
+		if (okay == 0){
+			//Over lay
+			*lights &= ~(~((unsigned short)~0 << numbits) << starting_light);
+			pattern <<= starting_light;
+			*lights |= pattern;
+		}
+	} while (okay != 0);
+}
+/**************************************************************
+Return 0 if input is valid,
+Otherwise: print error message and return -1
+*/
+int validate_input(char *input, int *start_bit, int *numbit, unsigned short * pattern) {
+	// trimg leading spaces
+	while ( isspace((unsigned char)*input) ) input++;
+	if (*input == 0) {// is input has only spaces
+		printf("\nYou did not enter anything! Try again!\n");
+		return -1;
 	}
-	if (pattern) {
-
+	char *sbit = strtok(input, " ");
+	char *ptrn = strtok(NULL, " ");
+	if (strtok(NULL, " ") > 0) {
+		printf("Too many entries on line! Try again!\n");
+		return -1;
 	}
-}
-void overlay(unsigned short* lights, unsigned short pattern) {
+	if (!sbit || !ptrn) {
+		if (!sbit)
+			printf("You did not enter starting light! Try again!\n");
+		if (!ptrn)
+			printf("You did not enter light pattern! Try again!\n");
+		return -1;
+	}
+	sbit[strspn(sbit, NUMERIC)] = 0;  // only get numeric values in a string
+	ptrn[strspn(ptrn, NUMERIC)] = 0;
+	if (*sbit == 0 || *ptrn == 0) {
+		printf("Please only enter numeric value (e.g 2 1101)! Try again!\n");
+		FLUSH;
+		return -1;
+	}
+	//Convert to number
+	*numbit = strlen(ptrn);
+	*start_bit = strtol(sbit, NULL, 0);
+	*pattern = strtol(ptrn, NULL, 2);
 
+	if ((*start_bit + 1) > NUM_LIGHTS) {
+		printf("Invalid starting light number! Try again (max is %d)\n", NUM_LIGHTS - 1);
+		return -1;
+	}
+	if ((NUM_LIGHTS - *start_bit) < *numbit) {
+		printf("Invalid light pattern. Number of lights should be less than %d (MAX LIGHTS - starting light)! Try again.\n", NUM_LIGHTS - *start_bit);
+		return -1;
+	}
+	if (*pattern == 0){
+		printf("Illegal on/off pattern! Try again!\n");
+		return -1;
+	}
+	return 0;
 }
-
+/**************************************************************
+Return 0 is string is digit, else return 1
+*/
+int is_digit(const char* input) {
+	int error = 0;
+	for (int i = 0; i < (int)strlen(input); i++) {
+		if (!isdigit(input[i])) {
+			error = 1;
+			break;
+		}
+	}
+	return error;
+}
 /**************************************************************
 Return 1 if bit_to_test is set and 0 if it is unset
 */
@@ -136,3 +213,123 @@ void printbits(unsigned short wrd){
 	printf("\n\n");
 	return;
 }
+
+/*
+SAMPLE OUTPUT
+
+
+==================== LIGHTING SYSTEM ===================
+1) Turn on all lights.
+2) Turn on center-stage lights ( 5 - 10)
+3) Turn on left-stage lights (11 - 15).
+4) Turn on right-stage lights (0 - 4).
+5) Turn off all lights.
+6) Turn off center-stage lights.
+7) Turn off left-stage lights.
+8) Turn off right-stage lights.
+9) Overlay ON/OFF pattern onto light configuration.
+10) Quit.
+=========================================================
+Enter menu choice: aw51
+Illegal menu selection! Try again!
+==================== LIGHTING SYSTEM ===================
+1) Turn on all lights.
+2) Turn on center-stage lights ( 5 - 10)
+3) Turn on left-stage lights (11 - 15).
+4) Turn on right-stage lights (0 - 4).
+5) Turn off all lights.
+6) Turn off center-stage lights.
+7) Turn off left-stage lights.
+8) Turn off right-stage lights.
+9) Overlay ON/OFF pattern onto light configuration.
+10) Quit.
+=========================================================
+Enter menu choice: 2
+Light settings: 0000011111100000
+
+==================== LIGHTING SYSTEM ===================
+1) Turn on all lights.
+2) Turn on center-stage lights ( 5 - 10)
+3) Turn on left-stage lights (11 - 15).
+4) Turn on right-stage lights (0 - 4).
+5) Turn off all lights.
+6) Turn off center-stage lights.
+7) Turn off left-stage lights.
+8) Turn off right-stage lights.
+9) Overlay ON/OFF pattern onto light configuration.
+10) Quit.
+=========================================================
+Enter menu choice: 4
+Light settings: 0000011111111111
+
+==================== LIGHTING SYSTEM ===================
+1) Turn on all lights.
+2) Turn on center-stage lights ( 5 - 10)
+3) Turn on left-stage lights (11 - 15).
+4) Turn on right-stage lights (0 - 4).
+5) Turn off all lights.
+6) Turn off center-stage lights.
+7) Turn off left-stage lights.
+8) Turn off right-stage lights.
+9) Overlay ON/OFF pattern onto light configuration.
+10) Quit.
+=========================================================
+Enter menu choice: 8
+Light settings: 0000011111100000
+
+==================== LIGHTING SYSTEM ===================
+1) Turn on all lights.
+2) Turn on center-stage lights ( 5 - 10)
+3) Turn on left-stage lights (11 - 15).
+4) Turn on right-stage lights (0 - 4).
+5) Turn off all lights.
+6) Turn off center-stage lights.
+7) Turn off left-stage lights.
+8) Turn off right-stage lights.
+9) Overlay ON/OFF pattern onto light configuration.
+10) Quit.
+=========================================================
+Enter menu choice: 9
+Enter starting light number and on/off pattern:  125as 15151
+Invalid starting light number! Try again (max is 15)
+Enter starting light number and on/off pattern: 10 1100
+Light settings: 0011001111100000
+
+==================== LIGHTING SYSTEM ===================
+1) Turn on all lights.
+2) Turn on center-stage lights ( 5 - 10)
+3) Turn on left-stage lights (11 - 15).
+4) Turn on right-stage lights (0 - 4).
+5) Turn off all lights.
+6) Turn off center-stage lights.
+7) Turn off left-stage lights.
+8) Turn off right-stage lights.
+9) Overlay ON/OFF pattern onto light configuration.
+10) Quit.
+=========================================================
+Enter menu choice: 9
+Enter starting light number and on/off pattern: 6 9
+Illegal on/off pattern! Try again!
+Enter starting light number and on/off pattern: 6
+You did not enter light pattern! Try again!
+Enter starting light number and on/off pattern: 6 10 foo
+Too many entries on line! Try again!
+Enter starting light number and on/off pattern: 6 101010
+Light settings: 0011101010100000
+
+==================== LIGHTING SYSTEM ===================
+1) Turn on all lights.
+2) Turn on center-stage lights ( 5 - 10)
+3) Turn on left-stage lights (11 - 15).
+4) Turn on right-stage lights (0 - 4).
+5) Turn off all lights.
+6) Turn off center-stage lights.
+7) Turn off left-stage lights.
+8) Turn off right-stage lights.
+9) Overlay ON/OFF pattern onto light configuration.
+10) Quit.
+=========================================================
+Enter menu choice: 10
+Press any key to continue . . .
+
+*/
